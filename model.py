121bd06400d67df1a53daf311a8407015d6b7fbb
ThomasBgn <31156703+ThomasBgn@users.noreply.github.com>
2017-09-02 10:18:44 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,22 @@
+#création de la classe.
 class Agent:
+
+	#première fonction pour dire bonjour.
 	def say_hello(self,first_name):
 		return "Bien le bonjour "+first_name +"!"
 
-	def __init__(self,agreeableness):
-		self.agreeableness = agreeableness
+	#seconde fonction, ici on travaille avec le dictionnaire "agent_attributes".
+	def __init__(self, agent_attributes):
+		#création d'une boucle pour récupérer l'ensemble des attributs et de leurs valeurs.
+		#fin de la boucle quand tous les items de agent_attributes sont vus.
+		for attr_name, attr_value in agent_attributes.items():
+			setattr(self, attr_name, attr_value)
 
-agent1 = Agent(0)
-agent2 = Agent(10)
-print(agent1.agreeableness,agent2.agreeableness)
+#Variable contenant le dictionnaire.
+agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": "false", "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+
+#création d'un objet.
+agent1 = Agent(agent_attributes)
+
+#affichage de l'attribut désiré.
+print(agent1.neuroticism)
